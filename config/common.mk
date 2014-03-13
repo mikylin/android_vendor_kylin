5fb400c64ff8955a051d0e977ba58c0f4e3306d6
zerojim kylin <kylinmod@gmail.com>
2014-03-13 16:11:33 +0800

    Fix config error

--- config/common.mk
+++ config/common.mk
@@ -99,7 +99,7 @@ PRODUCT_COPY_FILES += \
 # KylinMod prebuilts
 PRODUCT_COPY_FILES += \
     vendor/kylin/prebuilt/common/app/Market.apk:system/app/Market.apk \
-    vendor/kylin/prebuilt/common/app/MarketExt.apk:system/app/MarketExt.apk
+    vendor/kylin/prebuilt/common/app/MarketExt.apk:system/app/MarketExt.apk \
     vendor/kylin/prebuilt/common/app/GameCenter.apk:system/app/GameCenter.apk \
     vendor/kylin/prebuilt/common/app/iFlyIME.apk:system/app/LatinIME.apk \
     vendor/kylin/prebuilt/common/app/Stats.apk:system/app/Stats.apk \
